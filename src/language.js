8c3a67e5791b86d3cae5b89b7eb6989ece454f70
doomsayer2 <fgrassinger@gmx.at>
2017-04-11 08:56:20 +0200

    Styling fixes and adds logo. (refs #1)

--- src/language.js
+++ src/language.js
@@ -1,8 +1,14 @@
 /**
  * Created by Florian Grassinger on Mon, 10 Apr 2017 08:54:58 GMT.
+ *
+ * Add your constants here or Strings in order to make it possible for future distributions to change the language.
+ * If all strings and texts are written in constants it's easier to change the language depending on the region settings.
  */
 /**
  * Created by Florian Grassinger on Mon, 10 Apr 2017 08:54:58 GMT.
+ *
+ * Add your constants here or Strings in order to make it possible for future distributions to change the language.
+ * If all strings and texts are written in constants it's easier to change the language depending on the region settings.
  */ export var APP_NAME = 'Valid';
-export var HELLO_WORLD = 'Hello Phovea World';
+export var HELLO_WORLD = 'Hello World this is VALID... or will be...';
 //# sourceMappingURL=language.js.map
--- src/language.ts
+++ src/language.ts
@@ -1,6 +1,9 @@
 /**
  * Created by Florian Grassinger on Mon, 10 Apr 2017 08:54:58 GMT.
+ *
+ * Add your constants here or Strings in order to make it possible for future distributions to change the language.
+ * If all strings and texts are written in constants it's easier to change the language depending on the region settings.
  */
 
 export const APP_NAME = 'Valid';
-export const HELLO_WORLD = 'Hello Phovea World';
+export const HELLO_WORLD = 'Hello World this is VALID... or will be...';
