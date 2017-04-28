8ff56e8b3915b93376d9f5969fa905f6c1ba5e8b
doomsayer2 <fgrassinger@gmx.at>
2017-04-28 08:57:41 +0200

    Adds the basic upload functionality and sankey implementation (refs #6, #1)

--- src/data_import.ts
+++ src/data_import.ts
@@ -5,12 +5,19 @@
 import * as events from 'phovea_core/src/event';
 import * as d3 from 'd3';
 import * as papaparse from 'papaparse';
-// import 'imports-loader?d3=d3!../lib/sankey.js';
+import * as $ from 'jquery';
+import 'imports-loader?d3=d3!../lib/sankey.js';
 import {MAppViews} from './app';
+
+//TODO (F & N): Fire event while file is loading that enables the busy view.
+//TODO (C): How to add a sankey:
+//           * Add import statement:      import 'imports-loader?d3=d3!../lib/sankey.js';
+//           * Initialize object e.g.     const sankey = (<any>d3).sankey();
 
 class DataImport implements MAppViews {
 
-  private $node;
+  private $node: d3.Selection<any>;
+  private $dropZoneCont: d3.Selection<any>;
 
   constructor(parent: Element, private options: any) {
     this.$node = d3.select(parent)
@@ -25,8 +32,7 @@ class DataImport implements MAppViews {
    */
   init() {
     d3.select('.dataVizView').classed('invisibleClass', true);
-    // const sankey = (<any>d3).sankey();
-    // console.log('Thats a sankey', sankey);
+
     this.build();
     this.attachListener();
 
@@ -39,13 +45,82 @@ class DataImport implements MAppViews {
    * Build the basic DOM elements
    */
   private build() {
-    console.log('Hello from papaparse', papaparse);
+    this.$dropZoneCont = this.$node.html(`
+    <div class='fileContainer'>
+    <center><h1>Upload your data here!!</h1></center>
+      <form class='form-inline well'>
+        <div class='form-group'>
+          <label for='files'>Upload a CSV file:</label>
+          <input type='file' id='files' class='form-control' accept='.csv' required />
+        </div>
+        <div class='form-group'>
+          <button type='submit' id='submit-file' class='btn btn-primary'>Upload File</button>
+        </div>
+      </form>
+      <div class="row"
+			    <div class="row" id="valuesList">
+			</div>
+    </div>
+    `);
+
   }
 
   /**
    * Attach the event listeners
    */
   private attachListener() {
+    this.$node.select('#submit-file')
+      .on('click', (e) => {
+        const filesInput = <HTMLInputElement>d3.select('#files').node();
+        this.handleFileUpload(filesInput)
+
+        //Necessary in order to prevent the reload of the page.
+        const evt = <MouseEvent>d3.event;
+        evt.preventDefault();
+        evt.stopPropagation();
+      });
+  }
+
+  private handleFileUpload(filesInput) {
+    papaparse.parse(filesInput.files[0], {
+      header: true,   //First row of parsed data is interpreted as field name
+      skipEmptyLines: true,   //Skips empty lines in .csv
+      complete: this.displayData,     //Exceutes once data is loaded
+      error: function(err, file)    //Executes if there is an error loading the file
+			{
+				console.log("ERROR:", err, file);
+			},
+      // step: this.displayData
+    });
+  }
+
+  private displayData(results) {
+    //Resize the table appropriate and add scroll area if necessary
+    d3.select('#valuesList')
+      .attr('max-width', ($(window).innerWidth() / 2))
+      .classed('scrollArea', true);
+
+		let table = `<table class='table'>`;
+		let data = results.data;
+
+    //Create the header of the table
+		table += '<thead>';
+		for (var k in results.data[0] ) {
+      table += '<th>' + k + '</th>';
+    }
+    table += '</thead>';
+
+		for (let i = 0; i < data.length; i++) {
+      let row = data[i];
+
+      table += '<tr>';
+		  for (let key in row) {
+        table += '<td>' + row[key] + '</td>';
+      }
+      table += '</tr>';
+		}
+		table += '</table>';
+		$("#valuesList").html(table);
   }
 
 }
