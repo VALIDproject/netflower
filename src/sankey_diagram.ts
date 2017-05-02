365b6bc66bdee43b006d20d34abb28b567721a8c
NChristina <Christina.Niederer@fhstp.ac.at>
2017-05-02 13:18:33 +0200

    Implement sankey diagram - work in progress #10

--- src/sankey_diagram.ts
+++ src/sankey_diagram.ts
@@ -53,10 +53,10 @@ class SankeyDiagram implements MAppViews {
 
   private buildSankey() {
 
-    console.log(this.$node);
+    // console.log(this.$node);
 
     const sankey = (<any>d3).sankey();
-    console.log('Sankey Object', sankey);
+    // console.log('Sankey Object', sankey);
 
     const units = '€';
 
@@ -65,10 +65,10 @@ class SankeyDiagram implements MAppViews {
      console.log('width',  widthNode);*/
 
     let widthNode = this.$node.select('.sankey_vis').node().getBoundingClientRect().width;
-    console.log('width',  widthNode);
+    // console.log('width',  widthNode);
 
     let heightNode = this.$node.select('.sankey_vis').node().getBoundingClientRect().height;
-    console.log('height',  heightNode);
+    //console.log('height',  heightNode);
 
     const margin = {top: 10, right: 80, bottom: 10, left: 80};
     const width =  widthNode  - margin.left - margin.right;
@@ -76,7 +76,6 @@ class SankeyDiagram implements MAppViews {
 
     // The "0" option enables zero-padding.
     //The comma (",") option enables the use of a comma for a thousands separator.
-    //????
     const formatNumber = d3.format(',.0f'),    // zero decimal places
       format = function(d) { return formatNumber(d) + " " + units; },
       color = d3.scale.category20();
@@ -93,8 +92,143 @@ class SankeyDiagram implements MAppViews {
       .size([width, height]);
 
     const path = sankey.link();
-    console.log('path', path);
-    
+    // console.log('path', path);
+
+    const json =  [
+      {'source':'Albertina',
+        "target":"Vienna Deluxe Magazine GmbH",
+        "value": 17801.84
+      },
+      {"source":"Albertina",
+        "target":"Wien Programm",
+        "value":5197.98
+      },
+      {"source":"Agrarmarkt Austria Marketing GesmbH",
+        "target":"Lebensart",
+        "value": 5702
+      },
+      {"source":"Bundeskanzleramt",
+        "target":"Wien Programm",
+        "value":5197.98
+      },
+      {'source':'Albertina',
+        "target":"Vienna Deluxe Magazine GmbH",
+        "value": 17801.84
+      },
+      {"source":"Albertina",
+        "target":"Der Standard",
+        "value":4500
+      },
+      {"source":"Agrarmarkt Austria Marketing GesmbH",
+        "target":"Woman",
+        "value": 59662.90
+      },
+      {"source":"Bundeskanzleramt",
+        "target":"Kleine Zeitung",
+        "value":75068
+      },
+      {"source":"Bundeskanzleramt",
+        "target":"Vienna Deluxe Magazine GmbH",
+        "value":6230
+      }
+    ];
+    //console.log(json);
+
+    let graph = {'nodes' : [], 'links' : []};
+    //console.log(graph);
+
+    json.forEach(function (d) {
+      //console.log("source: ", d.source, "target: ", d.target);
+      graph.nodes.push({ 'name': d.source });//all Nodes
+      graph.nodes.push({ 'name': d.target });//all Nodes
+      graph.links.push({ 'source': d.source,
+        'target': d.target,
+        'value': +d.value })
+    });
+
+    console.log('Graph Array', graph.nodes);
+
+    graph.nodes = d3.keys(d3.nest()
+      .key(function (d) {
+        console.log('Data nest', d, d.name);
+        return d.name;
+
+      })
+      .map(graph.nodes));
+
+    //console.log('GraphNodes',graph.nodes);
+
+    let text;
+    graph.links.forEach(function (d, i) {
+      graph.links[i].source = graph.nodes.indexOf(graph.links[i].source);
+      graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
+    });
+
+    //console.log('GraphLinks',graph.links);
+
+    graph.nodes.forEach(function (d, i) {
+      graph.nodes[i] = { 'name': d };
+    });
+
+    sankey
+      .nodes(graph.nodes)
+      //.links(linksorted)
+      .links(graph.links)
+      .layout(10);//32
+
+
+    let link = svg.append('g').selectAll('.link')
+      .data(graph.links)
+      .enter().append('path')
+      .attr('class', 'link')
+      .attr('d', path)
+      .style('stroke-width', function(d) { return Math.max(1, d.dy); })
+      //reduce edges crossing
+      .sort(function(a, b) { return b.dy - a.dy; });
+
+    // add the link titles HOVER
+    link.append('title')
+      .text(function(d) {
+        return d.source.name + ' → ' +
+          d.target.name + '\n' + format(d.value); });
+
+
+    // add in the nodes
+    let node = svg.append('g').selectAll('.node')
+      .data(graph.nodes)
+      .enter().append('g')
+      .attr('class', 'node')
+      .attr('transform', function(d) {
+        return 'translate(' + d.x + ',' + d.y + ')';
+      });
+
+
+    // add the rectangles for the nodes
+    node.append('rect')
+      .attr('height', function(d) { return d.dy; })
+      .attr('width', sankey.nodeWidth())
+      .style('fill', 'orange')
+
+      //Title rectangle
+      .append('title')
+      .text(function(d) {
+        return d.name + '\n' + format(d.value); });
+
+    // add in the title for the nodes
+    node.append('text')
+      .attr('x', 10)
+      .attr('y', function(d) { return d.dy / 2; }) //in der Mitte des Nodes positionieren
+      .attr('dy', '.35em')
+      .attr('text-anchor', 'start')
+      .attr('transform', null)
+      .text(function(d) { return d.name +  ' '  +  '€ ' +  d.value; })
+      .filter(function(d) { return d.x < width / 2; })
+      //Node Text left
+      .attr('x', 2 + sankey.nodeWidth())
+      .attr('text-anchor', 'end');
+
+
+
   }
 
 }
