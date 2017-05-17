b8137f34b8dae593a7d99db41a605a5c2ce578e2
doomsayer2 <fgrassinger@gmx.at>
2017-05-17 09:53:00 +0200

    Adds localforage and data storage and loading example. (refs #14) EXECUTE: 'npm install'

--- src/global_settings.ts
+++ src/global_settings.ts
@@ -5,12 +5,14 @@
 import * as events from 'phovea_core/src/event';
 import * as d3 from 'd3';
 import * as bootbox from 'bootbox';
+import * as localforage from 'localforage';
 import {MAppViews} from './app';
 
 class GlobalSettings implements MAppViews {
 
   private $node: d3.Selection<any>;
   private $genericControls: d3.Selection<any>;
+  private promiseData;
 
   constructor(parent: Element, private options: any) {
     this.$node = d3.select(parent)
@@ -40,6 +42,7 @@ class GlobalSettings implements MAppViews {
 
     this.$genericControls = d3.select('.genericControls').html(`
       <button type='button' id='backBtn' class='btn btn-secondary'><i class='fa fa-hand-o-left'>&nbsp;</i>Back</button>
+      <button type='button' id='getData' class='btn btn-primary'><i class='fa fa-hand-o-left'>&nbsp;</i>Get Data</button>
     `);
   }
 
@@ -53,16 +56,34 @@ class GlobalSettings implements MAppViews {
         bootbox.confirm({
           className: 'dialogBox',
           title: 'Information',
-          message: 'Upon hitting the OK button, you will be redirected to the data upload page. ' +
-          'Note!! This will reload the page and the previous data will be lost.',
+          message: `Upon hitting the <strong>OK</strong> button, you will be redirected to the data upload page.<br/>
+          <strong>NOTE:</strong> This will reload the page and the previous data will be lost!!<br/><br/>
+          Be sure you don't lose anything important or save your progress before you proceed.`,
           callback: function(result) {
             if (result) {
+              localStorage.clear();
+              localforage.clear();
               //Force reload and loose all data
               location.reload(true);
             } else {
               return;
             }
           }
+        });
+
+        const evt = <MouseEvent>d3.event;
+        evt.preventDefault();
+        evt.stopPropagation();
+      });
+
+    this.$node.select('#getData')
+      .on('click', (e) => {
+        this.promiseData = localforage.getItem('data').then((value) => {
+          return value;
+        });
+
+        this.promiseData.then(function (data) {
+          console.log('DATA:::::::: ', data);
         });
 
         const evt = <MouseEvent>d3.event;
