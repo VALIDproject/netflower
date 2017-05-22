c7f92ca1d50f9c173fd0f73fb0803e8575b4cd5c
doomsayer2 <fgrassinger@gmx.at>
2017-05-22 19:44:34 +0200

    Fine tuneing of sankey diagram and refactoring. Also implementation of enhanced word wrapping function. (closes #10)

--- src/sankey_diagram.ts
+++ src/sankey_diagram.ts
@@ -1,29 +1,31 @@
 /**
-* Created by Florian on 12.04.2017.
-*/
+ * Created by Florian on 12.04.2017.
+ */
 
 import * as events from 'phovea_core/src/event';
 import * as d3 from 'd3';
-import {MAppViews} from './app';
+import * as localforage from 'localforage';
 import 'imports-loader?d3=d3!../lib/sankey.js';
 import {AppConstants} from './app_constants';
+import {MAppViews} from './app';
+import {d3TextWrap} from './utilities';
 
 class SankeyDiagram implements MAppViews {
 
   private $node;
-
+  private nodesToShow: number = 20;
 
   constructor(parent: Element, private options: any) {
     this.$node = d3.select(parent)
-    .append('div')
-    .classed('sankey_diagram', true);
+      .append('div')
+      .classed('sankey_diagram', true);
   }
 
   /**
-  * Initialize the view and return a promise
-  * that is resolved as soon the view is completely initialized.
-  * @returns {Promise<SankeyDiagram>}
-  */
+   * Initialize the view and return a promise
+   * that is resolved as soon the view is completely initialized.
+   * @returns {Promise<SankeyDiagram>}
+   */
   init() {
     this.build();
     this.attachListener();
@@ -34,20 +36,19 @@ class SankeyDiagram implements MAppViews {
 
 
   /**
-  * Build the basic DOM elements
-  */
+   * Build the basic DOM elements
+   */
   private build() {
     this.$node.append('div').attr('class', 'left_bars');
     this.$node.append('div').attr('class', 'sankey_vis');
     this.$node.append('div').attr('class', 'right_bars');
-
-    //this.buildSankey();
   }
 
   /**
-  * Attach the event listeners
-  */
+   * Attach the event listeners
+   */
   private attachListener() {
+
     events.on(AppConstants.EVENT_DATA_PARSED, (evt, data) => {
       //Draw Sankey Diagram
       this.buildSankey(data);
@@ -56,66 +57,61 @@ class SankeyDiagram implements MAppViews {
 
 
   private buildSankey(json) {
-
-    let sankey = (<any>d3).sankey();
-
+    const that = this;
+    const sankey = (<any>d3).sankey();
     const units = '€';
 
     let widthNode = this.$node.select('.sankey_vis').node().getBoundingClientRect().width;
-    // console.log('width',  widthNode);
-
     let heightNode = this.$node.select('.sankey_vis').node().getBoundingClientRect().height;
-    //console.log('height',  heightNode);
 
-    const margin = {top: 10, right: 120, bottom: 10, left: 120};
+    const margin = { top: 10, right: 120, bottom: 10, left: 120 };
     const width =  widthNode  - margin.left - margin.right;
     const height = heightNode - margin.top - margin.bottom;
 
-    // The "0" option enables zero-padding.
-    //The comma (",") option enables the use of a comma for a thousands separator.
+    //The "0" option enables zero-padding. The comma (",") option enables the use of a comma for a thousands separator.
     const formatNumber = d3.format(',.0f'),    // zero decimal places
-    format = function(d) { return formatNumber(d) + " " + units; },
-    color = d3.scale.category20();
+      format = function(d) { return formatNumber(d) + ' ' + units; }; //Display number with unit sign
 
-    // append the svg canvas to the page
-    const svg = d3.select(".sankey_vis").append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-    .attr('transform','translate(' + margin.left+ ',' + margin.top + ')');
+    //Append the svg canvas t the page
+    const svg = d3.select('.sankey_vis').append('svg')
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform','translate(' + margin.left+ ',' + margin.top + ')');
 
+    //Set the diagram properties
     sankey.nodeWidth(35)
-    .nodePadding(20)
-    .size([width, height]);
+      .nodePadding(20)
+      .size([width, height]);
 
     const path = sankey.link();
 
-    //Group Data
+    //Group Data (by quartal)
     let nest = (<any>d3).nest()
-    .key(function (d) {return d.quartal;})
-    .entries(json);
+      .key(function (d) {return d.quartal;})
+      .entries(json);
 
     let graph = {'nodes' : [], 'links' : []};
-    //console.log(graph);
 
     nest.forEach(function (d, i ) {
-      //console.log('d,i,values, key', d, i, d.values, d.key);
       if (d.key === '20151') {
-        //console.log('rechtstraeger', d.values[0].rechtstraeger);
-        for(var _v = 0; _v < 20; _v++) {
+        for(var _v = 0; _v < that.nodesToShow; _v++) {;
           //console.log(_v, d);
           graph.nodes.push({ 'name': d.values[_v].rechtstraeger });//all Nodes
           graph.nodes.push({ 'name': d.values[_v].mediumMedieninhaber });//all Nodes
           graph.links.push({ 'source': d.values[_v].rechtstraeger,
-          'target': d.values[_v].mediumMedieninhaber,
-          'value': +d.values[_v].euro });
+            'target': d.values[_v].mediumMedieninhaber,
+            'value': + d.values[_v].euro });
         }
       }
     });
 
+    //d3.keys - returns array of keys from the nest function
+    //d3.nest - groups the values of an array by the given key
+    //d3.map - constructs a new map and copies all enumerable properties from the specified object into this map.
     graph.nodes = (<any>d3).keys((<any>d3).nest()
-    .key((d) => {return d.name;})
-    .map(graph.nodes));
+      .key((d) => {return d.name;})
+      .map(graph.nodes));
 
     let text;
     graph.links.forEach(function (d, i) {
@@ -123,43 +119,43 @@ class SankeyDiagram implements MAppViews {
       graph.links[i].target = graph.nodes.indexOf(graph.links[i].target);
     });
 
-
+    //This makes out of the array of strings a array of objects with the key 'name'
     graph.nodes.forEach(function (d, i) {
       graph.nodes[i] = { 'name': d };
     });
 
+    //Basic parameters for the diagram
     sankey
-    .nodes(graph.nodes)
-    //.links(linksorted)
-    .links(graph.links)
-    .layout(10);
+      .nodes(graph.nodes)
+      //.links(linksorted)
+      .links(graph.links)
+      .layout(10);
 
 
     let link = svg.append('g').selectAll('.link')
-    .data(graph.links)
-    .enter().append('path')
-    .attr('class', 'link')
-    .attr('d', path)
-    .style('stroke-width', function(d) { return Math.max(1, d.dy); })
-    //reduce edges crossing
-    .sort(function(a, b) { return b.dy - a.dy; });
+      .data(graph.links)
+      .enter().append('path')
+      .attr('class', 'link')
+      .attr('d', path)
+      .style('stroke-width', function(d) { return Math.max(1, d.dy); })
+      //reduce edges crossing
+      .sort(function(a, b) { return b.dy - a.dy; });
 
-    link
-    .on("click", function(d) {
+
+    //Add the link titles - Hover Path
+    link.append('title')
+      .text(function(d) {
+        return d.source.name + ' → ' +
+          d.target.name + '\n' + format(d.value);
+      });
+
+    //Add the on 'click' listener for the links
+    link.on('click', function(d) {
       events.fire(AppConstants.EVENT_CLICKED_PATH, d, json);
     });
 
-
-
-    // add the link titles - Hover Path
-    link.append('title')
-    .text(function(d) {
-      return d.source.name + ' → ' +
-      d.target.name + '\n' + format(d.value); });
-
-
-      // add in the nodes
-      let node = svg.append('g').selectAll('.node')
+    //Add in the nodes
+    let node = svg.append('g').selectAll('.node')
       .data(graph.nodes)
       .enter().append('g')
       .attr('class', 'node')
@@ -167,79 +163,77 @@ class SankeyDiagram implements MAppViews {
         return 'translate(' + d.x + ',' + d.y + ')';
       });
 
-
-      // add the rectangles for the nodes
-      node.append('rect')
+    //Add the rectangles for the nodes
+    node.append('rect')
       .attr('height', function(d) { return d.dy; })
       .attr('width', sankey.nodeWidth())
       .style('fill', '#DA5A6B')
       //Title rectangle
       .append('title')
       .text(function(d) {
-        return d.name + '\n' + format(d.value); });
+        return d.name + '\n' + format(d.value);
+      });
 
-        //add in the title for the nodes
-        let heading =  node.append('g').append('text')
-        .attr('x', 45)
-        .attr('y', function(d) { return d.dy / 2; }) //in der Mitte des Nodes positionieren
-        .attr('dy', '.2em')
-        .attr('text-anchor', 'start')
-        .attr('transform', null)
-        .text(function(d) { return d.name; })
-        .filter(function(d) { return d.x < width / 2; })
-        //Node Text left
-        .attr('x', -45 + sankey.nodeWidth())
-        .attr('text-anchor', 'end');
+    //Add in the title for the nodes
+    let heading = node.append('g').append('text')
+      .attr('x', 45)
+      .attr('y', function(d) { return d.dy / 2; }) //Place in middle of node
+      .attr('dy', '.2em')
+      .attr('text-anchor', 'start')
+      .text(function(d) { return d.name; })
+      .filter(function(d, i) { return d.x < width / 2; })
+      //Node Text left if filter function is true
+      .attr('x', -45 + sankey.nodeWidth())
+      .attr('text-anchor', 'end');
 
-        //width of Text
-        let widthText = this.$node.selectAll('text').node().getBoundingClientRect().width;
+    //TODO: FG: Call the function with .call or .each d3TextWrap and pass depenendt arguments
+    // //Wrap the text
+    // let widthText = this.$node.selectAll('text').node().getBoundingClientRect().width;
+    //
+    // if(widthText >= 165 ) {
+    //   this.wrap(this.$node.selectAll('text'), 60);
+    // }
+  }
 
-        if(widthText >= 165 ) {
-          //console.log('heading', heading);
-          this.wrap(heading, widthText);
-        }
-      }
-      //splitting Text to fit in svg frame
-      private wrap (text, widthText) {
-        //console.log('wrapping');
-        text.each(function() {
-          //console.log('selections', d3.selectAll('text'));
-          let text = d3.select(this),
-          words = text.text().split(/\s+/).reverse(),
-          word,
-          line = [],
-          lineNumber = 0,
-          lineHeight = 1.1, // ems
-          y = text.attr('y'),
-          dy = parseFloat(text.attr('dy')),
-          tspan = text.text(null).append('tspan')
-          .attr('x', 0).attr('y', y)
-          .attr('dy', dy + 'em');
+  /**
+   * This function wraps the text in order to fit it to the svg size.
+   * @param text object to wrap
+   * @param widthText width of the text object
+   *
+   * @see: from http://bl.ocks.org/mbostock/7555321
+   */
+  // private wrap(text, width) {
+  //   text.each(function() {
+  //     let text = d3.select(this),
+  //       words = text.text().split(/\s+/).reverse(),
+  //       word,
+  //       line = [],
+  //       lineNumber = 0,
+  //       lineHeight = 1, // ems
+  //       y = text.attr("y"),
+  //       dy = parseFloat(text.attr("dy")) || 0;
+  //
+  //     let tspan = (text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em") as any);
+  //     while (word = words.pop()) {
+  //       line.push(word);
+  //       tspan.text(line.join(" "));
+  //       if (tspan.node().getComputedTextLength() > width) {
+  //         line.pop();
+  //         tspan.text(line.join(" "));
+  //         line = [word];
+  //         tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", (++lineNumber * lineHeight + dy) + "em").text(word);
+  //       }
+  //     }
+  //   });
+  // }
+}
 
-          //console.log('text', text, 'words', words, 'y', y, 'dy', dy, 'tspan', tspan, 'widthText', widthText);
-          while (word = words.pop()) {
-            line.push(word);
-            tspan.text(line.join(' '));
-
-            if (widthText >= 165) {
-              line.pop();
-              tspan.text(line.join(' '));
-              line = [word];
-              tspan = text.append('tspan').attr('x', -15).attr('y', y)
-              .attr('dy', ++lineNumber * lineHeight + dy + 'em')
-              .text(word);
-            }
-          }
-        });
-      }
-    }
-
-    /**
-    * Factory method to create a new SankeyDiagram instance
-    * @param parent
-    * @param options
-    * @returns {SankeyDiagram}
-    */
-    export function create(parent: Element, options: any) {
-      return new SankeyDiagram(parent, options);
-    }
+/**
+ * Factory method to create a new SankeyDiagram instance
+ * @param parent
+ * @param options
+ * @returns {SankeyDiagram}
+ */
+export function create(parent: Element, options: any) {
+  return new SankeyDiagram(parent, options);
+}
