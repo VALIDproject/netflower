0e2ea9bf01c95c6a4686d9ffdff5598b81c7fc3e
NChristina <Christina.Niederer@fhstp.ac.at>
2017-05-22 13:40:30 +0200

    Implement detail Sankey work in progress #10

--- src/sankey_detail.ts
+++ src/sankey_detail.ts
@@ -56,7 +56,6 @@ class SankeyDetail implements MAppViews {
 
   private openDetails (clickedPath, json) {
     //console.log('openDetails - true', clickedPath);
-
     let details = this.$node;
     details.attr('transform', 'translate(' + 600 + ',' + 500 + ')')
     .attr('width', 600 + 'px')
@@ -66,59 +65,68 @@ class SankeyDetail implements MAppViews {
 
     console.log(json);
 
-    //generate Data
-
     let euroOverTime = (<any>d3).nest()
     .key(function (d) {return d.quartal; })
     .key(function (d) {return d.rechtstraeger; })
     .rollup(function (v) {
       //console.log('v', v);
       let sum = (<any>d3).sum(v, function (d){ return d.euro});
-     //console.log('sum', sum);
-      return sum; })
-    .map(json);
+      //console.log('sum', sum);
+      return [sum]})
+      .map(json);
 
-    console.log('key Quartal and key rechtstraeger', euroOverTime);
+      console.log(euroOverTime, 'length', Object.keys(euroOverTime).length);
 
-    // let sumQuartal = (<any>d3).nest()
-    // .key(function (d) {return d.quartal; })
-    // // .rollup(function (v) {
-    // //   //console.log('v', v);
-    // //   let sum = (<any>d3).sum(v, function (d){ return d.euro});
-    // //  //console.log('sum', sum);
-    // //   return sum; })
-    // .map(euroOverTime);
-    //
-    // console.log('Sum over Quartal', sumQuartal);
+      // for(var i = 0; i <= Object.keys(euroOverTime).length; i++) {
+      //
+      //   console.log(i, euroOverTime[i]);
+      //
+      // }
 
+      for (let key in euroOverTime) {
+        if (euroOverTime.hasOwnProperty(key)) {
 
+          if(key === '20151') {
+          console.log(key + " -> " + euroOverTime[key]);
 
+          }
 
-
-
-    // details.selectAll(".bar")
-    //     .data(json)
-    //   .enter().append("rect")
-    //     .attr("class", "bar")
-    //     .attr("x", function(d) { return x(d.salesperson); })
-    //     .attr("width", x.bandwidth())
-    //     .attr("y", function(d) { return y(d.sales); })
-    //     .attr("height", function(d) { return height - y(d.sales); });
+          //console.log(key + " -> " + euroOverTime[key]);
+        }
+      }
 
 
 
 
+      // let sumQuartal = (<any>d3).nest()
+      // .key(function (d) {return d.quartal; })
+      // // .rollup(function (v) {
+      // //   //console.log('v', v);
+      // //   let sum = (<any>d3).sum(v, function (d){ return d.euro});
+      // //  //console.log('sum', sum);
+      // //   return sum; })
+      // .map(euroOverTime);
+      //
+      // console.log('Sum over Quartal', sumQuartal);
+
+      // details.selectAll(".bar")
+      //     .data(json)
+      //   .enter().append("rect")
+      //     .attr("class", "bar")
+      //     .attr("x", function(d) { return x(d.salesperson); })
+      //     .attr("width", x.bandwidth())
+      //     .attr("y", function(d) { return y(d.sales); })
+      //     .attr("height", function(d) { return height - y(d.sales); });
 
 
-
+    }
   }
-}
-/**
-* Factory method to create a new SankeyDiagram instance
-* @param parent
-* @param options
-* @returns {SankeyDetail}
-*/
-export function create(parent: Element, options: any) {
-  return new SankeyDetail(parent, options);
-}
+  /**
+  * Factory method to create a new SankeyDiagram instance
+  * @param parent
+  * @param options
+  * @returns {SankeyDetail}
+  */
+  export function create(parent: Element, options: any) {
+    return new SankeyDetail(parent, options);
+  }
