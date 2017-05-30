fe3a44a8b33ef7532ee9dc4e923acc930ed955e9
Alexander Rind <rind@ifs.tuwien.ac.at>
2017-05-30 17:23:16 +0200

    filter and aggregate data (target still hardcoded)

--- src/sparklineBarChart.ts
+++ src/sparklineBarChart.ts
@@ -4,11 +4,13 @@
 
 import * as events from 'phovea_core/src/event';
 import * as d3 from 'd3';
+import * as localforage from 'localforage';
 import {MAppViews} from './app';
 
 class SparklineBarChart implements MAppViews {
 
   private $node;
+  private promiseData;
 
  constructor(parent: Element, private options: any) {
     this.$node = d3.select(parent)
@@ -38,6 +40,23 @@ class SparklineBarChart implements MAppViews {
     this.$node.html(`
     <p>Hello World</p>
        `);
+
+            //This is how we retrieve the data. As it's loaded async it is only available as promise.
+        //We can save the promise thoug in a global variable and get the data later if we need
+        this.promiseData = localforage.getItem('data').then((value) => {
+          return value;
+        });
+
+        //Within the {} the data is available for usage
+        this.promiseData.then(function (data) {
+          // TODO get legalEnt/medium name into this scope
+          var filtered_data = data.filter(function(d) { return d.mediumMedieninhaber == "Kurier"; });
+          var aggregated_data = d3.nest()
+            .key(function(d) { return d.quartal; })
+            .rollup(function(v) { return d3.sum(v, function(d) { return d.euro; }})
+            .entries(filtered_data);
+          console.log('spark data: ', JSON.stringify(aggregated_data));
+        });
   }
 
   /**
@@ -50,10 +69,10 @@ class SparklineBarChart implements MAppViews {
 }
 
 /**
- * Factory method to create a new Barchart instance
+ * Factory method to create a new SparklineBarChart instance
  * @param parent
  * @param options
- * @returns {Barchart}
+ * @returns {SparklineBarChart}
  */
 export function create(parent: Element, options: any) {
   return new SparklineBarChart(parent, options);
