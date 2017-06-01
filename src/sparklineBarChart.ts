e154a1441a544d36f1b238240ba816777cb854bd
Alexander Rind <rind@ifs.tuwien.ac.at>
2017-06-01 17:03:38 +0200

    simple non-interactive bar chart for media (tackles #8)

--- src/sparklineBarChart.ts
+++ src/sparklineBarChart.ts
@@ -31,15 +31,14 @@ class SparklineBarChart implements MAppViews {
     return Promise.resolve(this);
   }
 
-
   /**
    * Build the basic DOM elements
    */
   private build() {
-        console.log("Now executing SparklineBarChart :-)");
-    this.$node.html(`
-    <p>This will turn into a sparkline barchart</p>
-       `);
+//        console.log("Now executing SparklineBarChart :-)");
+//     this.$node.html(`
+//     <p>This will turn into a sparkline barchart</p>
+//        `);
 
             //This is how we retrieve the data. As it's loaded async it is only available as promise.
         //We can save the promise thoug in a global variable and get the data later if we need
@@ -56,8 +55,43 @@ class SparklineBarChart implements MAppViews {
             .rollup(function(v) { return d3.sum(v, function(d) { return d.euro; }})
             .entries(filtered_data);
           console.log('spark data: ', JSON.stringify(aggregated_data));
+
+    let width = 120;
+    let height = 40;
+
+    let x = d3.scale.ordinal().rangeRoundBands([0, width], .05);
+    let y = d3.scale.linear().range([height, 0]);
+    let xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+    let yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left")
+    .ticks(10);
+
+    x.domain(aggregated_data.map(function(d,i) { return d.key; }));
+    y.domain([0, d3.max(aggregated_data, function(d) { return d.values; })]);
+
+    let svg = d3.select("div.right_bars").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+svg.selectAll("bar")
+      .data(aggregated_data)
+    .enter().append("rect")
+      .style("fill", "steelblue")
+      .attr("x", function(d, i) { return x(d.key); })
+      .attr("width", x.rangeBand())
+      .attr("y", function(d) { return y(d.values); })
+      .attr("height", function(d) { return height - y(d.values); });
+
         });
   }
+
+  /*
+  private timeSeries(table: Array<Object>, source: String) {
+    return null;
+  } */
 
   /**
    * Attach the event listeners
