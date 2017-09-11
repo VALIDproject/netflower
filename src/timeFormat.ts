bb4d73f3d96406ea1ea288957bf4dba187de0d27
Alexander Rind <rind@ifs.tuwien.ac.at>
2017-09-11 13:25:53 +0200

    time format for months & some comments to document code

--- src/timeFormat.ts
+++ src/timeFormat.ts
@@ -1,3 +1,8 @@
+import * as d3 from 'd3';
+
+const inFormatMonth = d3.time.format("%Y%m");
+const outFormatMonth = d3.time.format("%b %Y");
+
 export default class TimeFormat {
 
     private static func: (raw: string) => string = passThrough;
@@ -10,12 +15,24 @@ export default class TimeFormat {
         return TimeFormat.func(raw.toString());
     }
 
+    /**
+     * configures time format based on column name of time column.
+     * Currently supports "quarter" and "month" and checks if these strings are contained in the column name.
+     * The default is to pass-through the raw value, which works well for e.g. "year".
+     * @param header
+     */
     public static setFormat(header: string) {
-        if (header === "Quarter") {
+        if (header.match(/quarter/i)) {
             TimeFormat.func = function (raw: string): string {
                 return raw.substr(0, 4) + " Q" + raw.substr(-1);
             }
             console.log("set to a quarter format");
+        } else if (header.match(/month/i)) {
+            TimeFormat.func = function (raw: string): string {
+                let date = inFormatMonth.parse(raw); // returns a Date
+                return outFormatMonth(date);
+            }
+            console.log("set to a month format");
         } else {
             TimeFormat.func = passThrough;
             console.log("set to default format");
