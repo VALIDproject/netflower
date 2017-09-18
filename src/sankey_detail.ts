ea839d46e6892455938d0041840d47b4a06a04a1
doomsayer2 <fgrassinger@gmx.at>
2017-09-18 10:03:23 +0200

    Fix for develop.

--- src/sankey_detail.ts
+++ src/sankey_detail.ts
@@ -1,6 +1,6 @@
 /**
-* Created by Florian on 12.04.2017.
-*/
+ * Created by Florian on 12.04.2017.
+ */
 
 import * as events from 'phovea_core/src/event';
 import * as d3 from 'd3';
@@ -28,10 +28,10 @@ class SankeyDetail implements MAppViews {
   }
 
   /**
-  * Initialize the view and return a promise
-  * that is resolved as soon the view is completely initialized.
-  * @returns {Promise<SankeyDetail>}
-  */
+   * Initialize the view and return a promise
+   * that is resolved as soon the view is completely initialized.
+   * @returns {Promise<SankeyDetail>}
+   */
   init() {
     // this.build();
     this.attachListener();
@@ -42,17 +42,18 @@ class SankeyDetail implements MAppViews {
 
 
   /**
-  * Build the basic DOM elements
-  */
+   * Build the basic DOM elements
+   */
   // private build() {
   //
   // }
 
   /**
-  * Attach the event listeners
-  */
+   * Attach the event listeners
+   */
   private attachListener() {
     events.on(AppConstants.EVENT_CLICKED_PATH, (evt, data, json, coordinates) => {
+      //console.log('Coordinaten, mouseclick evenet', coordinates, coordinates[0], coordinates[0]);
       if (this.clicked <= 1 ) {
         this.drawDetails(data, json, coordinates);
         ++this.clicked;
@@ -69,177 +70,179 @@ class SankeyDetail implements MAppViews {
   }
 
   /**
-  * This method cleans up the view by removing all generated graphs and charts.
-  */
+   * This method cleans up the view by removing all generated graphs and charts.
+   */
   private closeDetail () {
     this.$node.select('svg.sankey_details').remove();
     this.$node.select('svg.sankey_details2').remove();
   }
 
   /**
-  * This method draws the bar chart over time for each selected node.
-  * @param clickedPath is the node which was clicked by the user
-  * @param json is the whole data set in order to retrieve all time points for the current node
-  */
-  private drawDetails (clickedPath, json) {
-    const margin = {top: 30 , right: 40, bottom: 30, left: 40},
+   * This method draws the bar chart over time for each selected node.
+   * @param clickedPath is the node which was clicked by the user
+   * @param json is the whole data set in order to retrieve all time points for the current node
+   */
+  private drawDetails (clickedPath, json, coordinates) {
+    let margin = {top: 50 , right: 60, bottom: 60, left: 60},
+      w = 400 - margin.left - margin.right,
+      h = 200 - margin.top - margin.bottom;
 
-    w = 400 - margin.left - margin.right,
-    h = 200 - margin.top - margin.bottom;
-
-    const sourceName = clickedPath.source.name;
-    const targetName = clickedPath.target.name;
-    const value = clickedPath.target.value;
+    let sourceName = clickedPath.source.name;
+    let targetName = clickedPath.target.name;
+    let value = clickedPath.target.value;
 
     //Tooltip for the bar chart
-    const tooltip = this.$node.append('div')
-    .attr('class', 'tooltip')
-    .style('opacity', 0)
-    .style('z-index', '200000');
-    
-    //Get width and height of sankey div to calculate position of svg
-    const widthSankeyDiv = (<any>d3).select('.sankey_vis').node().getBoundingClientRect().width;
-    const heightSankeyDiv = (<any>d3).select('.sankey_vis').node().getBoundingClientRect().height;
-    const windowWidth = window.innerWidth;
-    const windowHeight = window.innerHeight;
+    let tooltip = this.$node.append('div')
+      .attr('class', 'tooltip')
+      .style('opacity', 0)
+      .style('z-index', '200000');
 
-    //Position of svg in the sankey_diagram div
-    const xpositionSvg = windowWidth / 2 - (w/2);
-    const ypositionSvg = coordinates[1] + h;
-    const newYPositionSvg = ypositionSvg + 10;
+    //get width and height of sankey div to calculate position of svg
+    let widthSankeyDiv = (<any>d3).select('.sankey_vis').node().getBoundingClientRect().width;
+    let heightSankeyDiv = (<any>d3).select('.sankey_vis').node().getBoundingClientRect().height;
+
+    let windowWidth = window.innerWidth;
+    let windowHeight = window.innerHeight;
+
+    //position of svg in the sankey_diagram div
+    //let xpositionSvg = widthSankeyDiv / 2 + 100;
+    let xpositionSvg = windowWidth / 2 - (w/2);
+    let ypositionSvg = coordinates[1] + h;
+    let newYPositionSvg = ypositionSvg + 10;
 
     if (this.drawSvg === 0) {
+
       this.$node.append('svg')
-      .attr('class', 'sankey_details')
-      .attr('transform', 'translate(' + xpositionSvg + ',' + newYPositionSvg + ')')
-      .attr('width', w + margin.left + margin.right + 'px')
-      .attr('height', h + margin.top + margin.bottom + 'px')
-      .style('background-color',  '#e0e0e0')
-      .style('z-index', '10000');
+        .attr('class', 'sankey_details')
+        .attr('transform', 'translate(' + xpositionSvg + ',' + newYPositionSvg + ')')
+        .attr('width', w + margin.left + margin.right + 'px')
+        .attr('height', h + margin.top + margin.bottom + 'px')
+        .style('background-color',  '#e0e0e0')
+        .style('z-index', '10000');
 
       this.$node.select('svg.sankey_details').append('g')
-      .attr('class', 'headingDetailSankey')
-      .style('z-index', '10001');
+        .attr('class', 'headingDetailSankey')
+        .style('z-index', '10001');
 
       this.$node.select('svg.sankey_details').select('g.headingDetailSankey')
-      .append('text')
-      .attr('x', 5)
-      .attr('y', 16)
-      .style('font-size', 11 + 'px')
-      .text(function(d) {
-        return sourceName + ' → ' + targetName + '\u00A0' +  dotFormat(value);
-      });
+        .append('text')
+        .attr('x', 5)
+        .attr('y', 16)
+        .style('font-size', 11 + 'px')
+        .text(function(d) {
+          return sourceName + ' → ' + targetName + '\u00A0' +  dotFormat(value);
+        });
 
       const maxTextWidth = (w + margin.left + margin.right - 50);
       const text = this.$node.select('.sankey_details').select('.headingDetailSankey').selectAll('text');
-      d3TextWrap(text, maxTextWidth, 5, 5);
+      d3TextWrap(text , maxTextWidth, 5, 5);
 
       this.toolbox = d3.select('svg.sankey_details')
-      .append('g')
-      .attr('class', 'toolbox')
-      .attr('transform', 'translate(' + 5 + ',' + 12 + ')');
+        .append('g')
+        .attr('class', 'toolbox')
+        .attr('transform', 'translate(' + 5 + ',' + 12 + ')');
 
       this.toolbox.append('text')
-      .attr('font-family', 'FontAwesome')
-      .text(function(d) { return ' ' + '\uf24a';})
-      .style('z-index', '200000')
-      .attr('x', '354')
-      .attr('y', '10')
-      .attr('class', 'addNotes')
-      .on('mouseover', function(d) {
-        tooltip.transition().duration(200).style('opacity', .9);
-        tooltip.html('Add Notes to this particular flow!')
-        .style('left', ((<any>d3).event.pageX -40) + 'px')
-        .style('top', ((<any>d3).event.pageY - 20) + 'px');
-      })
-      .on('mouseout', function(d) {
-        tooltip.transition().duration(500).style('opacity', 0);
-      })
-      .on('click', function(d){
-        alert('Save NOTES in a feature version!!');
-      });
+        .attr('font-family', 'FontAwesome')
+        .text(function(d) { return ' ' + '\uf24a';})
+        .style('z-index', '200000')
+        .attr('x', '354')
+        .attr('y', '10')
+        .attr('class', 'addNotes')
+        .on('mouseover', function(d) {
+          tooltip.transition().duration(200).style('opacity', .9);
+          tooltip.html('Add Notes to this particular flow!')
+            .style('left', ((<any>d3).event.pageX -40) + 'px')
+            .style('top', ((<any>d3).event.pageY - 20) + 'px');
+        })
+        .on('mouseout', function(d) {
+          tooltip.transition().duration(500).style('opacity', 0);
+        })
+        .on('click', function(d){
+          alert('Save NOTES in a feature version!!');
+        });
 
       ++this.drawSvg;
+
     } else {
       this.$node.append('svg')
-      .attr('class', 'sankey_details2')
-      .attr('transform', 'translate(' + xpositionSvg + ',' + (newYPositionSvg + 20) + ')')
-      .attr('width', w + margin.left + margin.right + 'px')
-      .attr('height', h + margin.top + margin.bottom + 'px')
-      .style('background-color',  '#e0e0e0')
-      .style('z-index', '10000');
+        .attr('class', 'sankey_details2')
+        .attr('transform', 'translate(' + xpositionSvg + ',' + (newYPositionSvg + 20) + ')')
+        .attr('width', w + margin.left + margin.right + 'px')
+        .attr('height', h + margin.top + margin.bottom + 'px')
+        .style('background-color',  '#e0e0e0')
+        .style('z-index', '10000');
 
       this.$node.select('svg.sankey_details2').append('g')
-      .attr('class', 'headingDetailSankey2')
-      .style('z-index', '10001');
+        .attr('class', 'headingDetailSankey2')
+        .style('z-index', '10001');
 
       this.$node.select('svg.sankey_details2').select('g.headingDetailSankey2')
-      .append('text')
-      .attr('x', 5)
-      .attr('y', 16)
-      .style('font-size', 11 + 'px')
-      .text(function(d) {
-        return sourceName + ' → ' + targetName + '\u00A0' +  dotFormat(value);
-      });
+        .append('text')
+        .attr('x', 5)
+        .attr('y', 16)
+        .style('font-size', 11 + 'px')
+        .text(function(d) {
+          return sourceName + ' → ' + targetName + '\u00A0' +  dotFormat(value);
+        });
 
       const maxTextWidth = (w + margin.left + margin.right - 50);
       const text = this.$node.select('.sankey_details2').selectAll('text');
-      d3TextWrap(text, maxTextWidth, 5, 5);
+      d3TextWrap(text , maxTextWidth, 5, 5);
 
       this.toolbox2 = d3.select('svg.sankey_details2')
-      .append('g')
-      .attr('class', 'toolbox2')
-      .attr('transform', 'translate(' + 5 + ',' + 12 + ')');
+        .append('g')
+        .attr('class', 'toolbox2')
+        .attr('transform', 'translate(' + 5 + ',' + 12 + ')');
 
       this.toolbox2.append('text')
-      .attr('font-family', 'FontAwesome')
-      .text(function(d) { return ' ' + '\uf24a';})
-      .style('z-index', '200000')
-      .attr('x', '354')
-      .attr('y', '10')
-      .attr('class', 'addNotes')
-      .on('mouseover', function(d) {
-        tooltip.transition().duration(200).style('opacity', .9);
-        tooltip.html('Add Notes to this particular flow!')
-        .style('left', ((<any>d3).event.pageX -40) + 'px')
-        .style('top', ((<any>d3).event.pageY - 20) + 'px');
-      })
-      .on('mouseout', function(d) {
-        tooltip.transition().duration(500).style('opacity', 0);
-      })
-      .on('click', function(d){
-        alert('Save NOTES in a feature version!!');
-      });
+        .attr('font-family', 'FontAwesome')
+        .text(function(d) { return ' ' + '\uf24a';})
+        .style('z-index', '200000')
+        .attr('x', '354')
+        .attr('y', '10')
+        .attr('class', 'addNotes')
+        .on('mouseover', function(d) {
+          tooltip.transition().duration(200).style('opacity', .9);
+          tooltip.html('Add Notes to this particular flow!')
+            .style('left', ((<any>d3).event.pageX -40) + 'px')
+            .style('top', ((<any>d3).event.pageY - 20) + 'px');
+        })
+        .on('mouseout', function(d) {
+          tooltip.transition().duration(500).style('opacity', 0);
+        })
+        .on('click', function(d){
+          alert('Save NOTES in a feature version!!');
+        });
       this.drawSvg = 0;
     }
 
     //Filter data based on the clicked path (sourceName and targetName) and store it
-    const path = json.filter((obj) => {
+    let path = json.filter((obj) => {
       return obj.sourceNode === sourceName && obj.targetNode === targetName;
     });
 
     //Data for the bar chart
-    const valueOverTime = {};
-    for(const key in path) {
+    let valueOverTime = {};
+    for(let key in path) {
       if(path.hasOwnProperty(key)) {
         valueOverTime[path[key].timeNode] = path[key];
       }
     }
-    const data = [];
-    for(const i in valueOverTime) {
-      if(valueOverTime.hasOwnProperty(i)) {
-        data.push({timeNode: +valueOverTime[i].timeNode, valueNode: +valueOverTime[i].valueNode});
-      }
+    let data = [];
+    for(let i in valueOverTime) {
+      data.push({timeNode: +valueOverTime[i].timeNode, valueNode: +valueOverTime[i].valueNode});
     }
 
     //X-Scale and equal distribution
-    const x = (<any>d3).scale.ordinal()
-    .rangeBands([0, w], 0.2);
-    //Y-Scale for the chart
-    const y = d3.scale.linear()
-    .range([h, 0]);
+    let x = (<any>d3).scale.ordinal()
+      .rangeBands([0, w], 0.2);
 
-    const timePoints = d3.set(
+    //Y-Scale for the chart
+    let y = d3.scale.linear()
+      .range([h, 0]);
+
+    let timePoints = d3.set(
       json.map(function (d: any) { return d.timeNode; })
     ).values().sort();
 
@@ -249,51 +252,52 @@ class SankeyDetail implements MAppViews {
     if (this.drawBarChart === 0) {
       //Add the svg for the bars and transform it slightly to be in position of the box
       this.detailSVG = d3.select('svg.sankey_details')
-      .append('g')
-      .attr('class', 'bars')
-      .attr('transform', 'translate(' + (margin.left + 10) + ',' + margin.top + ')');
+        .append('g')
+        .attr('class', 'bars')
+        .attr('transform', 'translate(' + (margin.left + 10) + ',' + margin.top + ')');
       ++this.drawBarChart;
+
     } else {
       //Add the svg for the bars and transform it slightly to be in position of the box
       this.detailSVG = d3.select('svg.sankey_details2')
-      .append('g')
-      .attr('class', 'bars')
-      .attr('transform', 'translate(' + (margin.left + 10) + ',' + margin.top + ')');
+        .append('g')
+        .attr('class', 'bars')
+        .attr('transform', 'translate(' + (margin.left + 10) + ',' + margin.top + ')');
       this.drawBarChart = 0;
     }
 
     //Add in the bar charts and the tooltips if user mouses over them.
     this.detailSVG.selectAll('.bar')
-    .data(data)
-    .enter()
-    .append('rect')
-    .attr('class', 'bar')
-    .attr('x', function(d, i) { return x(d.timeNode); })
-    .attr('width', x.rangeBand())
-    .attr('y', function(d) { return y(d.valueNode); }) // h - y(d.valueNode);
-    .attr('height', function(d) { return y(0) - y(d.valueNode); })
-    .on('mouseover', function(d) {
-      tooltip.transition().duration(200).style('opacity', .9);
-      tooltip.html(dotFormat(d.valueNode))
-      .style('left', ((<any>d3).event.pageX -40) + 'px')
-      .style('top', ((<any>d3).event.pageY - 20) + 'px');
-    })
-    .on('mouseout', function(d) {
-      tooltip.transition().duration(500).style('opacity', 0);
-    });
+      .data(data)
+      .enter()
+      .append('rect')
+      .attr('class', 'bar')
+      .attr('x', function(d, i) { return x(d.timeNode); })
+      .attr('width', x.rangeBand())
+      .attr('y', function(d) { return y(d.valueNode); }) // h - y(d.valueNode);
+      .attr('height', function(d) { return y(0) - y(d.valueNode); })
+      .on('mouseover', function(d) {
+        tooltip.transition().duration(200).style('opacity', .9);
+        tooltip.html(dotFormat(d.valueNode))
+          .style('left', ((<any>d3).event.pageX -40) + 'px')
+          .style('top', ((<any>d3).event.pageY - 20) + 'px');
+      })
+      .on('mouseout', function(d) {
+        tooltip.transition().duration(500).style('opacity', 0);
+      });
 
     //Define the axes and draw them
-    const xAxis = d3.svg.axis().scale(x)
-    .tickFormat(TimeFormat.format)
-    .orient('bottom');
+    let xAxis = d3.svg.axis().scale(x)
+      .tickFormat(TimeFormat.format)
+      .orient('bottom');
 
-    const yAxis = d3.svg.axis().scale(y)
-    .orient('left');
+    let yAxis = d3.svg.axis().scale(y)
+      .orient('left');
 
     let xAxisElement = this.detailSVG.append('g')
-    .attr('class', 'x axis')
-    .attr('transform', 'translate(0,' + h + ')')
-    .call(xAxis);
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + h + ')')
+      .call(xAxis);
 
     xAxisElement.selectAll('.tick').selectAll('text')
       .attr('y', 0)
@@ -304,29 +308,31 @@ class SankeyDetail implements MAppViews {
       .style('font-size', 9 + 'px');
 
     this.detailSVG.append('g')
-    .attr('class', 'y axis')
-    .call(yAxis.ticks(4).tickFormat(d3.format(',')));
+      .attr('class', 'y axis')
+      .call(yAxis.ticks(4).tickFormat(d3.format(',')));
 
     //Append the close button or link to the SVG
-    const close = this.detailSVG.append('g').attr('class', 'closeLink');
+    let close = this.detailSVG.append('g').attr('class', 'closeLink');
     close.append('text')
-    .attr('font-family', 'FontAwesome')
-    .text(function(d) { return ' ' + '\uf00d';})
-    .style('z-index', '200000')
-    .attr('font-size', 14 + 'px')
-    .attr('x', '310')
-    .attr('y', '-28')
-    .on('click', function (d) {
-      events.fire(AppConstants.EVENT_CLOSE_DETAIL_SANKEY, d);
-    });
+      .attr('font-family', 'FontAwesome')
+      .text(function(d) { return ' ' + '\uf00d';})
+      .style('z-index', '200000')
+      .attr('font-size', 14 + 'px')
+      .attr('x', '310')
+      .attr('y', '-28')
+      .on('click', function (d) {
+        events.fire(AppConstants.EVENT_CLOSE_DETAIL_SANKEY, d);
+      });
   }
+
+
 }
 /**
-* Factory method to create a new SankeyDiagram instance
-* @param parent
-* @param options
-* @returns {SankeyDetail}
-*/
+ * Factory method to create a new SankeyDiagram instance
+ * @param parent
+ * @param options
+ * @returns {SankeyDetail}
+ */
 export function create(parent: Element, options: any) {
   return new SankeyDetail(parent, options);
 }
