c7ab503b6c6cec22b053e1a9bcd0cce9607a8b8e
doomsayer2 <fgrassinger@gmx.at>
2018-05-25 11:39:33 +0200

    Adds the donor dataset for download. (closes #179)

--- src/app_constants.ts
+++ src/app_constants.ts
@@ -37,9 +37,10 @@ export class AppConstants {
   static SANKEY_TOP_MARGIN = 10;
   static SANKEY_NODE_PADDING = 20;
 
-  //FILE DOWNLOADS
+  // FILE DOWNLOADS
   static ASYLUM_FILE = 'https://dl.dropboxusercontent.com/s/cr3iu0adtb77de6/Asylum_Seekers_Data.csv?dl=0';
   static FARM_FILE = 'https://dl.dropboxusercontent.com/s/zunih3hkcooh1gm/Farm_Subsidies_Data.csv?dl=0';
   static MEDIA_FILE = 'https://dl.dropboxusercontent.com/s/34ev5sr6u3xdisq/Media_Transperency_Data.csv?dl=0';
   static FILE4 = 'https://dl.dropboxusercontent.com/s/k4dhuh7hnmoclzf/Simple_Data.csv?dl=0';
+  static OECD_FILE = 'https://dl.dropboxusercontent.com/s/cvigz33c3g8h5be/Aid_Payments_OECD.csv?dl=0'
 }
