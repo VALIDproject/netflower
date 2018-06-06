1db81467a68527e4e9876e32becaaeb5310fbbd3
doomsayer2 <fgrassinger@gmx.at>
2018-06-06 09:56:00 +0200

    Adds the new logo to the site as well as relevant icons. (closes #182)

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@
 import 'file-loader?name=index.html!extract-loader!html-loader!./index.html';
 import 'file-loader?name=404.html-loader!./404.html';
 import 'file-loader?name=robots.txt!./robots.txt';
+import 'file-loader?name=favicon.ico!./favicon.ico';
+import 'file-loader?name=favicon.png!./favicon.png';
 import 'phovea_ui/src/_bootstrap';
 import 'phovea_ui/src/_font-awesome';
 import './style.scss';
