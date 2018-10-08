648b472a95d7ebdc8ccd64d10ecbc5cf20ec7e01
doomsayer2 <fgrassinger@gmx.at>
2018-10-08 10:30:15 +0200

    Tags saved in local storage and exported now fast.

--- src/export.ts
+++ src/export.ts
@@ -64,18 +64,7 @@ export default class Export implements MAppViews {
         if (fileName === '' || fileName === null) {
           fileName = 'No File Name.csv';
         }
-        // Store the tags to appropriate objects
-        let newStorageData = Export.currentData.map((val) => {
-          const sourceHash = Export.entityTagFilterRef
-            .getTagsByName(Export.currentData, val.sourceNode).values().join('|');
-          const targetHash = Export.mediaTagFilterRef
-            .getTagsByName(Export.currentData, val.targetNode).values().join('|');
-          return {
-            sourceNode: val.sourceNode, targetNode: val.targetNode, timeNode: val.timeNode,
-            valueNode: val.valueNode, sourceTag: sourceHash, targetTage: targetHash,
-            attribute1: val.attribute1, attribute2: val.attribute2
-          }
-        });
+        const newStorageData = Export.currentData;
         (newStorageData as Array<Object>).unshift(columnLabels);
         this.exportCSVFile(newStorageData, fileName.slice(0, -4));
       } else {
@@ -99,12 +88,12 @@ export default class Export implements MAppViews {
         const currentTime = [];
         d3.select('#timeInfoHeader').selectAll('span').each(function (d, i) {
           currentTime.push(d3.select(this).text());
-        })
+        });
         // Attributes that are set at the moment
         let currentAttrs = [];
         d3.select('#attribute1').selectAll('span').each(function (d, i) {
           currentAttrs.push(d3.select(this).text());
-        })
+        });
         currentAttrs = currentAttrs.filter(Boolean); // Needed to remove empty strings
 
         // Add the meta data as the first line of the csv
@@ -145,15 +134,17 @@ export default class Export implements MAppViews {
    * @returns {string} of the delimited object
    */
   private convertToCSV(objArray): string {
-    let array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
     let str = '';
     for (let i = 0; i < array.length; i++) {
       let line = '';
       for (const index in array[i]) {
-        if (line != '') {
-          line += ';';
+        if (array[i].hasOwnProperty(index)) {
+          if (line !== '') {
+            line += ';';
+          }
+          line += array[i][index];
         }
-        line += array[i][index];
       }
       str += line + '\r\n';
     }
@@ -176,10 +167,10 @@ export default class Export implements MAppViews {
     if (navigator.msSaveBlob) { // IE 10+
       navigator.msSaveBlob(blob, exportedFilenmae);
     } else {
-      var link = document.createElement('a');
+      const link = document.createElement('a');
       if (link.download !== undefined) { // feature detection
         // Browsers that support HTML5 download attribute
-        var url = URL.createObjectURL(blob);
+        const url = URL.createObjectURL(blob);
         link.setAttribute('href', url);
         link.setAttribute('download', exportedFilenmae);
         link.style.visibility = 'hidden';
